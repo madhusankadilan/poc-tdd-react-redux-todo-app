1ab6d4181c2449a7cc757de0fa28faf8a6d50bfc
m_dilan <m.dilan@directfn.com>
2022-08-22 00:33:18 +0530

    NEW | Sidebar unit test cases and related functionality added |

--- src/components/SideBar/SideBar.test.tsx
+++ src/components/SideBar/SideBar.test.tsx
@@ -1,51 +1,111 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SideBar from "./SideBar";
 
-describe("Side Bar Component Test Cases", () => {
-    describe("Side Bar Component Initial Render", () => {
-        test("render add toto text", () => {
-            render(<SideBar />);
+// setup function
+function setup(jsx: JSX.Element) {
+    return {
+        user: userEvent.setup(),
+        ...render(jsx),
+    };
+}
 
-            const paragraphElement = screen.getByText("Add Todo", {
+describe("Initial Render", () => {
+    test("render add toto text", () => {
+        render(<SideBar />);
+
+        const paragraphElement = screen.getByText("Add Todo", {
+            exact: true,
+        });
+
+        expect(paragraphElement).toHaveClass("sidebar-title");
+        expect(paragraphElement).toBeInTheDocument();
+    });
+
+    test("render title text box with empty value and having class 'title'", () => {
+        render(<SideBar />);
+
+        const inputElement = screen.getByPlaceholderText("Title", {
+            exact: true,
+        });
+
+        expect(inputElement).toHaveClass("title");
+        expect(inputElement).toHaveValue("");
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    test("render description text box with empty value and having class 'description'", () => {
+        render(<SideBar />);
+
+        const textareaElement = screen.getByPlaceholderText("Description", {
+            exact: true,
+        });
+
+        expect(textareaElement).toHaveClass("description");
+        expect(textareaElement).toHaveValue("");
+        expect(textareaElement).toBeInTheDocument();
+    });
+
+    test("render add todo button with class 'add-btn' and disabled when loading", () => {
+        render(<SideBar />);
+
+        const buttonElement = screen.getByRole("button", {
+            name: "ADD",
+        });
+
+        expect(buttonElement).toHaveClass("add-btn");
+        expect(buttonElement).toBeDisabled();
+        expect(buttonElement).toBeInTheDocument();
+    });
+});
+
+describe("Event Handling", () => {
+    test("onChange title field", async () => {
+        const { user } = setup(<SideBar />);
+        const inputElement: HTMLInputElement = screen.getByPlaceholderText(
+            "Title",
+            {
                 exact: true,
-            });
+            }
+        );
 
-            expect(paragraphElement).toHaveClass("sidebar-title");
-            expect(paragraphElement).toBeInTheDocument();
+        await user.type(inputElement, "Title");
+        expect(inputElement).toHaveValue("Title");
+        // expect(inputElement.value).toBe("Test Title");
+    });
+
+    test("onChange description field", async () => {
+        const { user } = setup(<SideBar />);
+        const textareaElement = screen.getByPlaceholderText("Description", {
+            exact: true,
         });
 
-        test("render title text box", () => {
-            render(<SideBar />);
+        await user.type(textareaElement, "Description");
+        expect(textareaElement).toHaveValue("Description");
+        // expect(textareaElement.value).toBe("Description");
+    });
 
-            const inputElement = screen.getByPlaceholderText("Title", {
+    test("ADD button should enabled when title and description boxes are not empty", async () => {
+        const { user } = setup(<SideBar />);
+
+        const buttonElement = screen.getByRole("button", {
+            name: "ADD",
+        });
+
+        const textareaElement = screen.getByPlaceholderText("Description", {
+            exact: true,
+        });
+
+        const inputElement: HTMLInputElement = screen.getByPlaceholderText(
+            "Title",
+            {
                 exact: true,
-            });
+            }
+        );
 
-            expect(inputElement).toHaveClass("title");
-            expect(inputElement).toBeInTheDocument();
-        });
-
-        test("render description text box", () => {
-            render(<SideBar />);
-
-            const textareaElement = screen.getByPlaceholderText("Description", {
-                exact: true,
-            });
-
-            expect(textareaElement).toHaveClass("description");
-            expect(textareaElement).toBeInTheDocument();
-        });
-
-        test("render add todo button", () => {
-            render(<SideBar />);
-
-            const buttonElement = screen.getByRole("button", {
-                name: "ADD",
-            });
-
-            expect(buttonElement).toHaveClass("add-btn");
-            expect(buttonElement).toBeInTheDocument();
-        });
+        await user.type(inputElement, "Title");
+        await user.type(textareaElement, "Description");
+        expect(buttonElement).not.toBeDisabled();
     });
 });
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import "./styles.css";
 
 const SideBar = () => {
+    const [todo, setTodo] = useState({
+        title: "",
+        description: "",
+    });
+
+    const handleTitleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+        setTodo((prevState) => ({
+            ...prevState,
+            title: ev.target.value,
+        }));
+    };
+
+    const handleDescriptionChange = (ev: ChangeEvent<HTMLTextAreaElement>) => {
+        setTodo((prevState) => ({
+            ...prevState,
+            description: ev.target.value,
+        }));
+    };
+
+    const handleAddTodo = () => {};
+
+    const isDisabled = () => {
+        if (todo.title && todo.description) {
+            return false;
+        }
+
+        return true;
+    };
+
     return (
         <aside className="sidebar">
             <div className="sidebar-header">
@@ -9,13 +38,30 @@ const SideBar = () => {
             </div>
             <div className="sidebar-content">
                 <div className="form-group-title">
-                    <input className="title" type="text" placeholder="Title" />
+                    <input
+                        className="title"
+                        type="text"
+                        placeholder="Title"
+                        value={todo.title}
+                        onChange={handleTitleChange}
+                    />
                 </div>
                 <div className="form-group-description">
-                    <textarea className="description" placeholder="Description"></textarea>
+                    <textarea
+                        className="description"
+                        placeholder="Description"
+                        value={todo.description}
+                        onChange={handleDescriptionChange}
+                    ></textarea>
                 </div>
                 <div className="form-group-add-btn">
-                    <button className="add-btn">ADD</button>
+                    <button
+                        className={`add-btn ${isDisabled() ? "disabled" : ""}`}
+                        onClick={handleAddTodo}
+                        disabled={isDisabled()}
+                    >
+                        ADD
+                    </button>
                 </div>
             </div>
         </aside>
